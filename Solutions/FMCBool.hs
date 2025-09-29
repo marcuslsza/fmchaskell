39bc55616fbebb2b2f355b1b4285ec0322a2f5a3
marcuslsza <marcuslimalsza@gmail.com>
2025-09-29 08:13:57 -0300

    bool feito

--- Solutions/FMCBool.hs
+++ Solutions/FMCBool.hs
@@ -22,71 +22,93 @@ import Prelude
 -- without using standard Haskell functions.
 -- (Hint: recursion is your friend!)
 
-data Bool = False | True
+data Bool = False | True 
 
 instance Show Bool where
 
-    show = undefined
+    show True = "True"
+    show False  = "False"
 
 instance Enum Bool where
+    toEnum 1 = True
 
-    toEnum  = undefined
-
-    fromEnum  = undefined
+    fromEnum False = 0
 
 -- conjunction (AND)
 (&&) :: Bool -> Bool -> Bool
-(&&) = undefined
+True && True = True
+False && _ = False
+_ && False = False
 
 infixr 3 &&
 
 -- disjunction (OR)
 (||) :: Bool -> Bool -> Bool
-(||) = undefined
+True || _ = True
+_ || True = True
+False || False = False
 
 infixr 2 ||
 
 -- NAND (Sheffer stroke)
 (/|\) :: Bool -> Bool -> Bool
-(/|\) = undefined
+True /|\ True = False
+False /|\ _ = True
+_ /|\ False = True
 
 infixr 2 /|\
 
 -- NOR (aka: Peirce arrow or Quine dagger)
 (\|/) :: Bool -> Bool -> Bool
-(\|/) = undefined
+False \|/ False = False
+True \|/ _ = False
+False \|/ _ = False
+
 
 infixr 2 \|/
 
 -- XOR (exclusive disjunction)
 (<=/=>) :: Bool -> Bool -> Bool
-(<=/=>) = undefined
+True <=/=> False = True
+False <=/=> True = True
+True <=/=> _ = False
+False <=/=> _ = False
 
 infixr 2 <=/=>
 
 -- boolean negation
 not :: Bool -> Bool
-not = undefined
+not False = True
+not True = False
 
 -- if-then-else expression
 ifThenElse :: Bool -> a -> a -> a
-ifThenElse = undefined
+ifThenElse True a b = a
 
 -- logical "implies"
 (==>) :: Bool -> Bool -> Bool
-(==>) = undefined
+True ==> True = True
+False ==> False = True
+True ==> False = False
+False ==> True = False
 
 infixr 1 ==>
 
 -- logical "implied by"
 (<==) :: Bool -> Bool -> Bool
-(<==) = undefined
+True <== True = True
+False <== False = True
+True <== False = False
+False <== True = False
 
 infixl 1 <==
 
 -- logical equivalence
 (<=>) :: Bool -> Bool -> Bool
-(<=>) = undefined
+True <=> True = True
+False <=> False = False
+True <=> False = False
+False <=> True = True
 
 infixr 1 <=>
 
